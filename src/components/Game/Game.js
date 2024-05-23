111b5922e05cb4b60c6609925a5cc1021898f50a
khushbukumari_300067093 <khushbu.kumari@myntra.com>
2024-05-23 07:34:35 +0530

    Guess Input component

---
+++ src/components/Game/Game.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import { sample } from '../../utils';
+import { WORDS } from '../../data';
+import GuessInput from '../GuessInput';
+
+// Pick a random word on every pageload.
+const answer = sample(WORDS);
+// To make debugging easier, we'll log the solution in the console.
+console.info({ answer });
+
+function Game() {
+  return <GuessInput />;
+}
+
+export default Game;
